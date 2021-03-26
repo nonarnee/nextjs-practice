3f5fd00275841800fd5d5dceeb21e13c079f65d8
nonarnee <nonarnee@gmail.com>
2021-03-26 16:04:40 +0900

    studying Assets, Metadata and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,15 +3,21 @@ import Link from "next/link";
 // If you need to link to an external page outside the Next.js app, just use an <a> tag without Link.
 // If you need to add attributes like, for example, className, add it to the a tag, not to the Link tag.
 
+import Head from "next/head";
+import Layout from "../../components/layout";
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
